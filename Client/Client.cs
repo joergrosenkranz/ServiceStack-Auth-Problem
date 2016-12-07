e81a5f81776d141bf4670b6b3747fc0f866dd2e4
Joerg Rosenkranz <joerg.rosenkranz@software.dell.com>
2016-12-07 17:19:48 +0100

    Demonstrate ServiceStack authentication problem

---
+++ Client/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Client
+{
+	class Client
+	{
+		static void Main()
+		{
+			var baseUrl = "http://localhost:1337/";
+			var authUrl = baseUrl + "auth/apphost?format=json";
+			var testUrl = baseUrl + "hello/John";
+
+			var handler = new HttpClientHandler
+			{
+				CookieContainer = new CookieContainer(),
+				UseDefaultCredentials = true
+			};
+			var client = new HttpClient(handler);
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Round {0}", i);
+
+				try
+				{
+					var authResult = client.PostAsync(authUrl, new StringContent("{}"))
+						.Result;
+
+					var data = client.GetAsync(testUrl).Result;
+
+					Console.WriteLine(data.IsSuccessStatusCode ? "Success!" : $"Failed with: {data.StatusCode}");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Failed with {0}", ex);
+				}
+			}
+		}
+	}
+}
---
+++ Server/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+using ServiceStack;
+using ServiceStack.Auth;
+
+class Program
+{
+	[Route("/hello/{Name}")]
+	public class Hello
+	{
+		public string Name { get; set; }
+	}
+
+	public class HelloResponse
+	{
+		public string Result { get; set; }
+	}
+
+	[Authenticate]
+	public class HelloService : Service
+	{
+		public object Any(Hello request)
+		{
+			return new HelloResponse { Result = "Hello, " + request.Name };
+		}
+	}
+
+	public class HelloAuthProvider : AuthProvider
+	{
+		private readonly ConcurrentDictionary<string, bool> _sessions = new ConcurrentDictionary<string, bool>();
+
+		public HelloAuthProvider()
+		{
+			Provider = "apphost";
+			AuthRealm = "/auth/" + Provider;
+		}
+
+		public override object Authenticate(IServiceBase authService, IAuthSession session, Authenticate request)
+		{
+			Console.WriteLine($"Authenticate called: session.IsAuthenticated = {session.IsAuthenticated}");
+
+			session.IsAuthenticated = true;
+
+			_sessions[session.Id] = true;
+
+			return new AuthenticateResponse
+				{
+					SessionId = session.Id
+				};
+		}
+
+		public override bool IsAuthorized(IAuthSession session, IAuthTokens tokens, Authenticate request = null)
+		{
+			return session?.Id != null && _sessions.ContainsKey(session.Id);
+		}
+	}
+
+	//Define the Web Services AppHost
+	public class AppHost : AppSelfHostBase
+	{
+		public AppHost()
+		  : base("HttpListener Self-Host", typeof(HelloService).Assembly) { }
+
+		public override void Configure(Funq.Container container)
+		{
+			Plugins.Add(new AuthFeature(
+				() => new AuthUserSession(),
+				new IAuthProvider[]
+					{
+						new HelloAuthProvider()
+					}));
+		}
+	}
+
+	//Run it!
+	static void Main(string[] args)
+	{
+		var listeningOn = args.Length == 0 ? "http://*:1337/" : args[0];
+		var appHost = new AppHost()
+			.Init()
+			.Start(listeningOn);
+
+		Console.WriteLine("AppHost Created at {0}, listening on {1}",
+			DateTime.Now, listeningOn);
+
+		Console.ReadKey();
+	}
+}
